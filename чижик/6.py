fea2923b9f14657c5d095e21b61ecf68e979bd2f
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2022-09-27 19:47:06 +0300

    Add files via upload

---
+++ чижик/6.py
@@ -0,0 +1,10 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # -*- coding: utf-8 -*-
+... a = int(input ())
+... b = int (input())
+... c = int (input())
+... d = int(input ())
+... if (a + b) % 2 == (c + d) % 2:
+... print ('ДА')
+... else:
---
+++ чижик/9.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # -*- coding: utf-8 -*-
+... n = int(input())
+... m = int(input())
+... k = int(input())
+... if k < n * m and ((k % n == 0) or (k % m == 0)):
+... print('ДА')
+... else:
