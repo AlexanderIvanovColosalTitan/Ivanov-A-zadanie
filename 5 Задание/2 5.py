78d46544dc9ebca3abf99a723789aa5e630b1b83
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2022-11-28 12:45:00 +0300

    Add files via upload

---
+++ 5 Задание/2 5.py
@@ -0,0 +1,9 @@
+def Zadanie(n):
+    q = 2
+    if n < 2:
+        print("Нужно больше двух")
+    for i in range (2, n):
+        if n%q == 0:
+            break
+        q += 1
+    print("Наименьший делитель - ",q)
