254c988b1bb41b3bdf30cce7d3fed3fa5c7a5406
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2022-11-28 19:40:51 +0300

    Delete 3 3.py

--- 3 Задание/3 3.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # -*- coding: utf-8 -*-
-... n = int(input())
-... hours = n % (60 * 24) // 60
-... minutes = n % 60
