e3004b2900dbb888fcab74cfb2f884d4d477b52d
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2022-11-28 13:11:04 +0300

    Add files via upload

---
+++ 7 Задание/1 7.py
@@ -0,0 +1,20 @@
+print('Введите длину массива - n в Zadanie(n) ')
+def Zadanie(n):
+    q = []
+    w = 0
+    k = 0
+    s = 0
+    for i in range(n):
+        print('Введите', i, 'элемент массива')
+        q.append(int(input()))
+    while n >= w:
+        k = 0
+        while n > k:
+            if (q[w] == q[k]) and (w != k):
+                print(q[w])
+                s = q[w]
+                q.remove(s)
+                k += 1
+            else:
+                k += 1
+        w += 1
