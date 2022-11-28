731e4d37289e9583285200eaa63d6045c2b367ad
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2022-11-28 13:08:25 +0300

    Add files via upload

---
+++ 6 Задание/1 6.py
@@ -0,0 +1,8 @@
+def Zadanie(s):
+    print(s[0].upper(), sep = '', end = '')
+    for i in range(1, len(s)):
+        if s[i-1] == ' ':
+            print(' ', sep = '', end = '')
+            print(s[i].upper(), sep = '', end = '')
+        else:
+            print(s[i], sep = '', end = '')
